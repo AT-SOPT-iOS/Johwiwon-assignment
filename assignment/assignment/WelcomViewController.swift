e6c6f3929cc94caf51f6fa0c9174da57af650800
hwidung <hwhwhw74@naver.com>
2025-04-25 21:55:42 +0900

    feat: SnapKit 적용

---
+++ assignment/assignment/WelcomViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment
+//
+//  Created by 조휘원 on 4/16/25.
+//
+
+import UIKit
+
+class WelcomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
