4ae54f75be14f28a1934f430f7b9c8da1ed907e8
hwidung <hwhwhw74@naver.com>
2025-06-03 14:31:37 +0900

    init: swift ui 프로젝트 세팅

---
+++ week7/week7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  week7
+//
+//  Created by 조휘원 on 6/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
